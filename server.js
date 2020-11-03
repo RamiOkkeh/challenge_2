432bce87f84abd8a8df2cd9eb25b12bf632dd202
RamiOkkeh <ramiokkeh@gmail.com>
2020-11-03 14:27:56 +0200

    oh god no!

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const multer = require('multer');
 const proccess = require('./proccess.js');
+var upload = multer({ dest: 'uploads/' });
 let app = express();
 
 app.use(express.static(__dirname + '/client'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/csvfy', (req, res) => {
+app.post('/csvfy', upload.single('json'), (req, res) => {
+  console.log(req.body.json);
   proccess(req.body.json, data => res.json(data)); 
 });
 
