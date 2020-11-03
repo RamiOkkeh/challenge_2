779ddcbf5c9fb50daa6f50e8e712696ca473cfd9
RamiOkkeh <ramiokkeh@gmail.com>
2020-11-03 11:46:39 +0200

    inital set up

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+const express = require('express');
 
+let app = express();
 
+app.use(express.static(__dirname + '/client'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/csvfy', (req, res) => {
+    
+});
+
+let port = 3001;
+
+app.listen(port, () => {
+  console.log('Listening on ' + port);
+});
